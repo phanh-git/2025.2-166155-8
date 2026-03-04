5b1d8d973ac86bb14e7bba45b37455b47480ccec
phanh-git <phanhforwork25@gmail.com>
2026-03-04 10:53:17 +0700

    Homework01

---
+++ Homework01/20235858-NguyenHoangThanhTu/MVC Caculator/src/CalculatorMain.java
@@ -0,0 +1,14 @@
+public class CalculatorMain {
+    
+    public static void main(String[] args) {
+    	
+    	CalculatorView theView = new CalculatorView();
+        
+    	CalculatorModel theModel = new CalculatorModel();
+        
+        CalculatorController theController = new CalculatorController(theView,theModel);
+        
+        theView.setVisible(true);
+        
+    }
+}
